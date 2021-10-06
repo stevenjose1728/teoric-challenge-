c01e3b6e5388a7d0a15cab1dd151248d00b9463e
Steven <stevenvarela98@gmail.com>
2021-10-06 16:15:42 -0400

    :wrench: teoric page

--- src/pages/Teoric.jsx
+++ src/pages/Teoric.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import TeoricItem from '../components/TeoricItem'
+import initialState from '../initialState'
 export default function Teoric() {
+    const {asks} = initialState
+    console.log('>>: initialState > ', initialState.asks)
     return (
         <div>
-            hola
+            {
+                asks.map(element => {
+                    return(
+                        <TeoricItem
+                            {... element}
+                            key={element.title}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
